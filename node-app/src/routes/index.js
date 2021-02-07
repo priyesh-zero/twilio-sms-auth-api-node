66d158276ee41c474d81caf072ecf82332910090
priyesh-zero <priyesh.zero@gmail.com>
2021-02-07 20:13:37 +0530

    Changed API To be Versioned

--- node-app/src/routes/index.js
+++ node-app/src/routes/index.js
@@ -2,11 +2,7 @@ const router = require("express").Router();
 const userRoutes = require("./api/User");
 const authRoutes = require("./api/Auth");
 
-router.use("/api/user", userRoutes);
-router.use("/api/auth", authRoutes);
-
-router.get("/", (_, __, next) => {
-  throw new Error("Custom Error");
-});
+router.use("/api/v1//user", userRoutes);
+router.use("/api/v1/auth", authRoutes);
 
 module.exports = router;
